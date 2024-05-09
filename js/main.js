7f04f6ebe55503473815c0662b764876ec453e43
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2024-05-09 11:16:12 +0300

    Удалён рекламный блок ads, удалил в первом листе propsList[0] текст 'undefined'

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const propsItem = document.querySelectorAll('.props__item');
-// console.log('propsItem: ', propsItem);
-
 const content = document.querySelectorAll('.content');
 const propsList = document.querySelectorAll('.props__list');
 
@@ -19,15 +17,17 @@ propsList[0].append(propsItem[2].append(propsItem[14]));
 propsList[2].append(propsItem[43]);
 propsList[2].append(propsItem[44]);
 
+const ads = document.querySelector('.ads');
+ads.remove();
 
 
+//------- мои поиски и размышления по вычислению текстого элемента "undefined" ------
 
+/* const html = document.documentElement;
+ console.log(html.childNodes[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[5].childNodes[11]); -  вычислил что это childNodes[11] */
 
-
-
-
-
-
+console.log(propsList[0].childNodes[11]);
+propsList[0].childNodes[11].remove();
 
 
 
