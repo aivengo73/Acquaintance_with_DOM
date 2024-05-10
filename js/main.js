9d984cc40e34fb0efb6b6ea095e025a13e18786f
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2024-05-10 21:49:19 +0300

    Исправил недочёты: Упорядочил списки и в каждом из них поменял текст h2 согласно образцу

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@
 const propsItem = document.querySelectorAll('.props__item');
 const content = document.querySelectorAll('.content');
 const propsList = document.querySelectorAll('.props__list');
+const items = document.querySelectorAll('.items');
+const item = document.querySelectorAll('.item');
+const h2 = document.querySelectorAll('.item__title');
+
+item[0].before(item[1]);
+item[2].before(item[3]);
+item[0].before(item[2]);
+item[2].after(item[3]);
 
 propsList[4].after(propsList[3]);
 content[3].append(propsList[4]);
-
-/* ---- Можно сделать так:
-propsList[0].append(propsItem[14]); 
-propsItem[2].append(propsItem[14]); 
- */
 
 propsList[0].append(propsItem[2].append(propsItem[14]));
 
@@ -20,14 +23,25 @@ propsList[2].append(propsItem[44]);
 const ads = document.querySelector('.ads');
 ads.remove();
 
-
-//------- мои поиски и размышления по вычислению текстого элемента "undefined" ------
-
-/* const html = document.documentElement;
- console.log(html.childNodes[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[5].childNodes[11]); -  вычислил что это childNodes[11] */
-
 console.log(propsList[0].childNodes[11]);
 propsList[0].childNodes[11].remove();
+
+ h2[2].textContent = 'This и прототипы объектов';
+ h2[4].textContent = 'ES6 и не только';
+ h2[1].textContent = 'Область видимости и замыкание';
+ h2[3].textContent = 'Асинхронная обработка и оптимизация';
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
